dee31699d020d0e6be2b5c3efc4ef9aeb9c89ff0
Alexey Gerasev <alexey.gerasev@gmail.com>
2020-09-19 08:56:39 +0700

    Move tensor functionality to trait

--- src/tensor/device.rs
+++ src/tensor/device.rs
@@ -1,7 +1,7 @@
 use crate::{
     Prm, Interop,
-    Tensor, Buffer,
-    DeviceBuffer, DeviceContext,
+    Tensor,
+    DeviceBuffer,
 };
 use std::{
     rc::Rc,
@@ -17,44 +17,27 @@ pub struct DeviceTensor<T: Prm + Interop> {
 }
 
 impl<T: Prm + Interop> DeviceTensor<T> {
-    /// Create tensor from shared buffer and shape
-    fn from_shared_buffer(rc_buffer: Rc<DeviceBuffer<T>>, shape: &[usize]) -> Self {
-        assert_eq!(rc_buffer.len(), shape.iter().product());
+    
+}
+
+impl<T: Prm + Interop> Tensor<T> for DeviceTensor<T> {
+    type Buffer = DeviceBuffer<T>;
+
+    unsafe fn from_shared_buffer_unchecked(rc_buffer: Rc<DeviceBuffer<T>>, shape: &[usize]) -> Self {
         Self {
             shape: shape.iter().cloned().collect(),
             buffer: rc_buffer,
         }
     }
-    /// Create tensor from specified buffer and shape
-    fn from_buffer(buffer: DeviceBuffer<T>, shape: &[usize]) -> Self {
-        Self::from_shared_buffer(Rc::new(buffer), shape)
-    }
 
-    /// Create unitialized tensor in the specified location.
-    pub unsafe fn new_uninit(context: &DeviceContext, shape: &[usize]) -> Self {
-        Self::from_buffer(DeviceBuffer::new_uninit_in(context, shape.iter().product()), shape)
-    }
-    /// Create tensor filled with value in the specified location.
-    pub fn new_filled(context: &DeviceContext, shape: &[usize], value: T) -> Self {
-        Self::from_buffer(DeviceBuffer::new_filled_in(context, shape.iter().product(), value), shape)
-    }
-    /// Create tensor filled with zeros in the specified location.
-    pub fn new_zeroed(context: &DeviceContext, shape: &[usize]) -> Self {
-        Self::new_filled(context, shape, T::zero())
-    }
-}
-
-impl<T: Prm + Interop> Tensor<T> for DeviceTensor<T> {
     fn shape(&self) -> &[usize] {
-        return self.shape.as_slice();
+        self.shape.as_slice()
     }
-    fn reshape(&self, shape: &[usize]) -> Self {
-        Self::from_shared_buffer(self.buffer.clone(), shape)
+
+    unsafe fn shared_buffer(&self) -> &Rc<DeviceBuffer<T>> {
+        &self.buffer
     }
-    fn load(&self, dst: &mut [T]) {
-        self.buffer.load(dst);
-    }
-    fn store(&mut self, src: &[T]) {
-        Rc::make_mut(&mut self.buffer).store(src);
+    unsafe fn shared_buffer_mut(&mut self) -> &mut Rc<DeviceBuffer<T>> {
+        &mut self.buffer
     }
 }
--- src/tensor/host.rs
+++ src/tensor/host.rs
@@ -1,5 +1,5 @@
 use crate::{
-    Prm, Buffer,
+    Prm,
     Tensor, HostBuffer,
 };
 use std::{
@@ -18,63 +18,42 @@ pub struct Iter<'a, T: Prm> {
 /// Tensor tries to reuse resources as long as possible and implements copy-on-write mechanism.
 pub struct HostTensor<T: Prm> {
     shape: Vec<usize>,
-    strides: Vec<isize>,
     buffer: Rc<HostBuffer<T>>,
 }
 
 impl<T: Prm> HostTensor<T> {
     /// Create unitialized tensor
     pub unsafe fn new_uninit(shape: &[usize]) -> Self {
-        Self::from_buffer(HostBuffer::new_uninit(shape.iter().product()), shape)
+        Self::new_uninit_in(&(), shape)
     }
     /// Create tensor filled with value on the specified hardware
     pub fn new_filled(shape: &[usize], value: T) -> Self {
-        Self::from_buffer(HostBuffer::new_filled(shape.iter().product(), value), shape)
+        Self::new_filled_in(&(), shape, value)
     }
     /// Create tensor filled with zeros on the specified hardware
     pub fn new_zeroed(shape: &[usize]) -> Self {
-        Self::new_filled(shape, T::zero())
+        Self::new_zeroed_in(&(), shape)
     }
 }
 
-impl<T: Prm> Tensor<T, HostBuffer<T>> for HostTensor<T> {
-    unsafe fn from_shared_buffer(rc_buffer: Rc<HostBuffer<T>>, shape: &[usize], strides: &[isize]) -> Self {
-        // FIXME: Check all elements are inside the buffer
-        //assert_eq!(rc_buffer.len(), shape.iter().product());
+impl<T: Prm> Tensor<T> for HostTensor<T> {
+    type Buffer = HostBuffer<T>;
+
+    unsafe fn from_shared_buffer_unchecked(rc_buffer: Rc<HostBuffer<T>>, shape: &[usize]) -> Self {
         Self {
             shape: shape.iter().cloned().collect(),
-            strides: strides.iter().cloned().collect(),
             buffer: rc_buffer,
         }
     }
 
-    unsafe fn from_buffer(buffer: HostBuffer<T>, shape: &[usize], strides: &[isize]) -> Self {
-        Self::from_shared_buffer(Rc::new(buffer), shape)
-    }
-
-    /// Create unitialized tensor
-    unsafe fn new_uninit_in(_: &(), shape: &[usize]) -> Self {
-        Self::from_buffer(HostBuffer::new_uninit(shape.iter().product()), shape)
-    }
-    /// Create tensor filled with value on the specified hardware
-    fn new_filled_in(_: &(), shape: &[usize], value: T) -> Self {
-        Self::from_buffer(HostBuffer::new_filled(shape.iter().product(), value), shape)
-    }
-    /// Create tensor filled with zeros on the specified hardware
-    fn new_zeroed_in(_: &(), shape: &[usize]) -> Self {
-        Self::new_filled(shape, T::zero())
-    }
-
     fn shape(&self) -> &[usize] {
-        return self.shape.as_slice();
+        self.shape.as_slice()
     }
-    fn reshape(&self, shape: &[usize]) -> Self {
-        Self::from_shared_buffer(self.buffer.clone(), shape)
+
+    unsafe fn shared_buffer(&self) -> &Rc<HostBuffer<T>> {
+        &self.buffer
     }
-    fn load(&self, dst: &mut [T]) {
-        self.buffer.load(dst);
-    }
-    fn store(&mut self, src: &[T]) {
-        Rc::make_mut(&mut self.buffer).store(src);
+    unsafe fn shared_buffer_mut(&mut self) -> &mut Rc<HostBuffer<T>> {
+        &mut self.buffer
     }
 }
